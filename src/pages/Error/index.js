6f1711f42eefb470d0f7e1b06fdeb8a6ec63e686
Sophia RK <130664417+s0pa-fria@users.noreply.github.com>
2024-04-19 11:26:12 -0300

    ..

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -4,4 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRotateBackward } from '@fortawesome/free-solid-svg-icons'; 
 import styles from './styles.module.css'; 
 
-// 4ª Digitação (Aqui)
+function Error() {//DEfine o componente funcional Erro
+    return(
+        <div className={styles.not_found}>
+            <h1>404</h1> {/* Redefine a titulo*/}
+            <h2>Oops! Página mão Encontrada</h2> {/* Reenderiza a mensagem de erro */}
+            <Link to='/'> {/* reenderiza a mensagem de erro */}
+
+                <FontAwesomeIcon icon={faArrowRotateBackward} size='lg' />{/* Vai para apagina inicial */}
+                Veja todos os filmes {/* Reenderiza o texto do link */}
+
+            </Link>
+        </div>
+    )
+}
+
+export default memo(Error); //Exporta o componente Erro, envolvido pela função 'memo' para otimização de renderização
